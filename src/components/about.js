b3a7d68faa8a7786c4c91c6a0354950cb900bc4d
luke <azathothbeats@gmail.com>
2018-04-13 23:35:05 -0400

    Fiddling with styles

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ const about = () => {
   return (
     <div className='About'>
       <div className='aboutImage'></div>
-      <p>
+      <p className='blurb'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
         incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
         exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
--- src/components/knownTech.js
+++ src/components/knownTech.js
@@ -7,21 +7,53 @@ class KnownTech extends Component {
       <div className='techContainer'>
         <ul className="techList">
 
-          <li className="techItem">
-            
-          </li>
+          <div className="techItem">
+            <h3>HTML5</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna 
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+              ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit 
+              esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+          </div>
 
-          <li className="techItem">
+          <div className="techItem">
+            <h3>CSS3</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna 
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+              ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit 
+              esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+          </div>
 
-          </li>
+          <div className="techItem">
+            <h3>JavaScript</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna 
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+              ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit 
+              esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+          </div>
 
-          <li className="techItem">
-
-          </li>
-
-          <li className="techItem">
-
-          </li>
+          <div className="techItem">
+            <h3>ReactJS</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna 
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+              ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit 
+              esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+          </div>
 
         </ul>
       </div>
